cda8a03e6fff03707a30ab425d16e7a8c5ad7aaa
dev-with-onyinye <onyinyeonyejiak@gmail.com>
2025-04-22 15:55:52 +0100

    fourth commit

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 entries = []
-
-TEMPLATE = '''
-<h1>My Dream Diary</h1>
-<form method="POST">
-    <label>Date: <input type="text" name="date"></label><br><br>
-    <label>Content:</label><br>
-    <textarea name="content" rows="5" cols="30"></textarea><br>
-    <button type="submit">Add Entry</button>
-</form>
-
-<h2>All Entries</h2>
-{% for entry in entries %}
-    <p><strong>Date:</strong> {{ entry.date }}<br>
-    <strong>Content:</strong> {{ entry.content }}</p>
-{% else %}
-    <p>No entries yet.</p>
-{% endfor %}
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def diary():
@@ -27,7 +9,7 @@ def diary():
         date = request.form['date']
         content = request.form['content']
         entries.append({'date': date, 'content': content})
-    return render_template_string(TEMPLATE, entries=entries)
+    return render_template('index.html', entries=entries)
 
 if __name__ == '__main__':
     app.run(debug=True)
